c8f2295470886830abd04ce8e98d6a9ad3280855
clarisse <clarissenicole3@gmail.com>
2023-06-05 18:54:46 -0700

    Model Encoders file created

--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from common.json import ModelEncoder
+import json
+from .models import AutomobileVO, Salesperson, Sale, Customer
+from .encoders import AutomobileVOEncoder, SalespersonEncoder, SaleEncoder, CustomerEncoder
 
-# Create your views here.
+
+@require_http_methods("GET")
+def api_list_automobileVOs(request):
